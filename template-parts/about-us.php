b54be2d8b5fa799c31fbfd4d653c684b74d94d77
Milan Poznan <milanpoznan@milan.local>
2020-06-17 21:40:21 +0200

    set webpack

---
+++ template-parts/about-us.php
@@ -0,0 +1,6 @@
+<section class="about-us">
+  <div class="about-us__wrapper">
+    <h1><?php the_field('o_nama_nasov'); ?></h1>
+    <div><?php the_field('o_nama_text'); ?></div>
+  </div>
+</section>
---
+++ template-parts/project-categories.php
@@ -0,0 +1,7 @@
+
+
+<section class="project-cat">
+  <div class="project-cat__wrapper">
+
+  </div>
+</section>
