b2783e93b4014d1684c6dadc81a0724549c9f4bc
Branislav <branislav@konstrukt.rs>
2020-07-11 14:28:47 +0200

    Archive blog and Single blog pages

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+
+get_header(); ?>
+
+<section class="container container--narrow">
+    <div class="blog-section">
+        <?php
+            while(have_posts()) {
+                the_post(); ?>
+                <div class="post-item">
+                    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                        <a class="post-item-img relative" href="<?php the_permalink(); ?>" style="background-image: url('<?php echo $image[0]; ?>')"></a>
+                    <?php endif; ?>
+                    <div class="meta-box">
+                        <h2 class="post-item-headline"><a class="gray-text" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <div class="generic-content">
+                            <p class="excerpt"><?php echo wp_trim_words(get_the_content(), 35); ?></p>
+                        </div>
+                        <p class="read-more no-b-m"><a class="blue-link" href="<?php the_permalink(); ?>">Read-more</a></p>
+                    </div>
+                </div>
+            <?php } ?>
+            <div class="pagination-wrapper text-center">
+                <div class="pagination">
+                <?php echo paginate_links(array(
+                        'base' => get_pagenum_link(1) . '%_%',
+                        'mid_size' => 2, 
+                        'format' => '/page/%#%',
+                        'prev_text'    => __(''),
+                        'next_text'    => __(''),
+                    )); ?>
+                </div>
+            </div>
+    </div>
+</section>
+
+<?php get_footer();
+
+?>
--- single.php
+++ single.php
@@ -9,29 +9,39 @@
 
 get_header();
 ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+			<div class="container">
+				<div class="single-post-wrapper">
+					<?php
+						while ( have_posts() ) :
+							the_post();
+							get_template_part( 'template-parts/content', get_post_type() );
+		
+						endwhile; // End of the loop.
+					?>
+					<div class="single-post-related">
+						<h2>Povezano</h2>
+						<ul class="related-list"> 
+							<?php $related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 2, 'post__not_in' => array($post->ID) ) );
+								if( $related ) foreach( $related as $post ) {
+									setup_postdata($post); ?>
+									<li>
+										<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+										<a class="post-item-img related relative" href="<?php the_permalink(); ?>" style="background-image: url('<?php echo $url ?>')"></a>										<h3 class="related-post-headline"><a class="gray-text" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+										<div class="generic-content">
+											<p class="excerpt"><?php echo wp_trim_words(get_the_content(), 35); ?></p>
+										</div>
+										<p class="read-more no-b-m"><a class="blue-link" href="<?php the_permalink(); ?>">Read-more</a></p>
+									</li>
+								<?php } ?>
+							<?php wp_reset_postdata(); 
+							?>
+						</ul>   
+					</div>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
-get_sidebar();
 get_footer();
