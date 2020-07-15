2dfc2bfdc4e39e82343a441439f34e666c69826a
Milan Poznan <milanpoznan@milan.local>
2020-07-15 15:06:19 +0200

    remove front page template

--- front-page.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="front-page">
-  <?php get_template_part('template-parts/hero'); ?>
-  <?php get_template_part('template-parts/services'); ?>
-  <?php get_template_part('template-parts/about-us'); ?>
-
-</div>
-<?php get_footer(); ?>
